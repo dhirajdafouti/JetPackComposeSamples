3a76c8a28c2be8bd3d8e7e06613ba40e96df2f75
CodingRock-Star <dhirajdafouti2020@gmail.com>
2024-10-02 22:19:28 +0530

    Changes for the dashboardScreen

---
+++ SampleExample1/app/src/main/java/com/android/sampleapplication1/screens/DashBoardScreen.kt
@@ -0,0 +1,21 @@
+package com.android.sampleapplication1.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun DashBoardScreen(navController: NavController){
+   Box(modifier = Modifier.fillMaxSize()){}
+
+}
+
+@Preview
+@Composable
+fun DashBoardScreenPreview(){
+    DashBoardScreen(navController = rememberNavController())
+}
