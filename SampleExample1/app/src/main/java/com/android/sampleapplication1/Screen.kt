3dd620b1753b8f69cd8c4fc9e5c94bd1aa251210
CodingRock-Star <dhirajdafouti2020@gmail.com>
2024-10-05 22:34:34 +0530

    removed the changes from sample1

--- SampleExample1/app/src/main/java/com/android/sampleapplication1/Screen.kt
+++
@@ -1,7 +0,0 @@
-package com.android.sampleapplication1
-
-sealed class Screen(val route: String) {
-    data object Landing : Screen("landing_screen")
-    data object Dashboard : Screen("dashboard_screen")
-    data object Product : Screen("product_screen")
-}
