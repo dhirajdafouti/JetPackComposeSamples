75ec4f3c8cb7f6e96898ff1f13403cd8be3b7852
CodingRock-Star <dhirajdafouti2020@gmail.com>
2024-12-17 21:10:31 +0530

    misc ui componenets

--- SampleExample1/app/src/main/java/com/android/sampleapplication1/screens/UiComponents.kt
+++ SampleExample1/app/src/main/java/com/android/sampleapplication1/screens/UiComponents.kt
@@ -3,13 +3,15 @@ package com.android.sampleapplication1.screens
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 
 @Composable
 fun MiscUiComponent(modifier: Modifier = Modifier) {
 
 }
 
-@Preview
+@PreviewLightDark
+@Preview(showBackground = true)
 @Composable
 private fun MiscUiComponentPreview() {
 
