44df4a0b50aafd7b6c598ae10b2795a5862f85e9
CodingRock-Star <dhirajdafouti2020@gmail.com>
2024-12-19 00:56:03 +0530

    Compose ui file

---
+++ SampleExample1/app/src/main/java/com/android/sampleapplication1/screens/NavigationDrawer.kt
@@ -0,0 +1,17 @@
+package com.android.sampleapplication1.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun NavigationDrawerCompose(modifier: Modifier = Modifier) {
+
+}
+
+
+@Preview
+@Composable
+private fun NavigationDrawerComposePreview() {
+
+}
---
+++ SampleExample1/app/src/main/java/com/android/sampleapplication1/screens/NavigationRails.kt
@@ -0,0 +1,16 @@
+package com.android.sampleapplication1.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun NavigationRailsCompose(modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+private fun NavigationRailsComposePreview() {
+    NavigationRailsCompose()
+}
