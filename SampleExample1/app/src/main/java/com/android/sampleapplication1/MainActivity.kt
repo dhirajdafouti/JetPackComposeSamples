073b9038010da1be2c6aedf4e5f44d273acb1bf3
CodingRock-Star <dhirajdafouti2020@gmail.com>
2024-12-23 00:23:31 +0530

    dialog

--- SampleExample1/app/src/main/java/com/android/sampleapplication1/MainActivity.kt
+++ SampleExample1/app/src/main/java/com/android/sampleapplication1/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.android.sampleapplication1.screens.BottomSheetsComposable
-import com.android.sampleapplication1.screens.BottomSheetsScaffoldComposable
-import com.android.sampleapplication1.screens.NavigationDrawerCompose
+import com.android.sampleapplication1.screens.UiComponenets
 import com.android.sampleapplication1.ui.theme.SampleApplication1Theme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +22,9 @@ class MainActivity : ComponentActivity() {
 //                    }
 //                }
               //  BottomSheetsComposable()
-                BottomSheetsScaffoldComposable()
+              //  BottomSheetsScaffoldComposable()
+                UiComponenets()
+               // DialogCompose()
             }
 
         }
